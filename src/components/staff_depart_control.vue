2d4b9f0fb1f3d1556c9d3292f8879825673fbc32
haylhf <haylhf@163.com>
2018-12-03 10:22:07 +0800

    添加staff签到左侧控件显示

---
+++ src/components/staff_depart_control.vue
@@ -0,0 +1,69 @@
+<template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml" >
+    <div style="width: 220px;height: 40px;margin-left: 120px;margin-top: 50px" >
+		<el-row >
+			<el-col :span="8" >
+               <span style="float: left;font-family: SourceHanSansCN-Medium;" >{{departInfo.departName}}</span >
+			</el-col >
+			<el-col :span="8" :offset="8" >
+                <span style="float: right;" >{{departInfo.currentValue}}/{{departInfo.totalValue}}</span >
+			</el-col >
+		</el-row >
+		<div style="margin-top: 5px;" >
+			<div class="progress"
+			     style="background-color: white;border: 1px solid #9D9D9D;height: 6px;border-radius: 0px" >
+				<div class="progress-bar" role="progressbar" :aria-valuenow="departInfo.currentValue"
+				     aria-valuemin="0" :aria-valuemax="departInfo.totalValue"
+				     :style="{ 'width': getPercent(departInfo.currentValue,departInfo.totalValue),
+				    'background-color': '#9D9D9D'}" >
+					<!--<span >40%</span>-->
+				</div >
+			</div >
+		</div >
+    </div >
+</template >
+
+<script >
+    var _this
+    export default {
+	    name: "StaffDepartControl",
+	    components: {},
+	    props: {
+		    departInfo: {
+			    type: Object,
+			    default: null
+		    },
+	    },
+	    data() {
+		    _this = this;
+		    return {
+		    }
+	    },
+	    methods: {
+		    getPercent(cValue, tValue)
+		    {
+			    let value = Math.floor((cValue / tValue) * 100);
+			    return `${value}%`;
+		    }
+	    },
+	    computed: {},
+	    filters: {},
+	    created: function () {
+
+	    },
+	    mounted: function () {
+
+	    },
+	    destroyed: function () {
+
+	    }
+    }
+
+</script >
+<style >
+    span {
+	    text-align: center;
+	    font-family: 'SquareFont';
+	    font-size: 18px;
+	    color: #AEAEAE;
+    }
+</style >
--- src/components/staff_page.vue
+++ src/components/staff_page.vue
@@ -1,26 +1,62 @@
 <template xmlns:v-on="http://www.w3.org/1999/xhtml" xmlns:v-bind="http://www.w3.org/1999/xhtml" >
-    <div >
+     <table style="width: 100%;height: 100%;position: fixed;" >
+            <tr style="height: 100%" >
+                <td width="30%" style="text-align: center;" >
+                    <div >
+                        <StaffDepartControl v-for="item in staffList"
+                                            :ref="staffDepart"
+                                            :departInfo="item" ></StaffDepartControl >
+                    </div >
+                </td >
+                <td style="text-align: center;" >
 
-    </div >
+                </td >
+                <td width="30%" style="text-align: right" >
 
+                </td >
+            </tr >
+        </table >
 </template >
 
 <script >
 
     import Vue from 'vue'
+    import StaffDepartControl from '../components/staff_depart_control.vue';
     var _this
     export default {
 	    name: "StaffPage",
-	    components: {},
+	    components: {
+		    StaffDepartControl,
+	    },
 	    data() {
 		    _this = this;
 		    return {
-
+			    staffDepart: {},
+			    staffList: [
+				    {
+					    departName: "人事部",
+					    currentValue: 40,
+					    totalValue: 100,
+				    },
+                    {
+                        departName: "产品部",
+                        currentValue: 50,
+                        totalValue: 100,
+                    },
+                    {
+                        departName: "开发部",
+                        currentValue: 28,
+                        totalValue: 100,
+                    },
+                    {
+                        departName: "投资部",
+                        currentValue: 80,
+                        totalValue: 100,
+                    }
+			    ],
 		    }
 	    },
-	    methods: {
-
-	    },
+	    methods: {},
 	    computed: {},
 	    filters: {},
 	    created: function () {
@@ -38,5 +74,8 @@
 <style >
     span {
 	    text-align: center;
+	    font-family: 'SquareFont';
+	    font-size: 18px;
+	    color: #AEAEAE;
     }
 </style >
